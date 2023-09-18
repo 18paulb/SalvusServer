4c79d1a8013ba9b2a09225d44270824ffdfa7d70
Brandon <bjpaul99@gmail.com>
2023-09-17 22:22:30 -0600

    trying to allow (security breach)

--- salvusbackend/settings.py
+++ salvusbackend/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'salvusbackend.herokuapp.com', 'salvusbackend-6f4cec5e1bd6.herokuapp.com','127.0.0.1']
+# ALLOWED_HOSTS = ['localhost', 'salvusbackend.herokuapp.com', 'salvusbackend-6f4cec5e1bd6.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
