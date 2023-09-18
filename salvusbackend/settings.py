c8b31fd4325a425c5daf896955c5d504823b6964
Brandon <bjpaul99@gmail.com>
2023-09-17 22:19:03 -0600

    tried to fix for deployment

--- salvusbackend/settings.py
+++ salvusbackend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'salvusbackend.herokuapp.com', 'https://salvusbackend-6f4cec5e1bd6.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'salvusbackend.herokuapp.com', 'salvusbackend-6f4cec5e1bd6.herokuapp.com','127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
