6e38bc71a4554352fcc2804e7c321682debfeead
Stanton Anthony <sa7235@gmail.com>
2023-09-21 21:47:24 -0600

    Fix authtoken stuff, make authentication endpoint, should cleanup later

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,6 +6,7 @@ from authentication import registerviews
 urlpatterns = [
     path('login', loginviews.login, name='login'),
     path('register', registerviews.register, name='register'),
+    path('verify-authtoken', loginviews.authenticate, name='authenticate')
 
     # other routes specific to app1
 ]
